38305520ad8ed5c2d548157ce45aa648ad310613
Michael Chan <michaelcychan@gmail.com>
2022-12-06 11:26:21 +0000

    add one more test to day 06

--- day-06/tuning-trouble/spec/check_duplicate_spec.rb
+++ day-06/tuning-trouble/spec/check_duplicate_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe 'check_duplicate' do
     actual = check_duplicate(arr_four)
     expect(actual).to eq(expected)
   end
+  it 'returns false for ["m", "p", "q", "m"]' do
+    arr_four = ["m", "p", "q", "m"]
+    expected = true
+
+    actual = check_duplicate(arr_four)
+    expect(actual).to eq(expected)
+  end
 end
